7f0f6e61da87080336edafbfe2f7dd0cc51e2aff
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-05-15 20:22:42 +0300

    creating a array of questions with objects inside in the index.js

---
+++ assets/index.js
@@ -0,0 +1,31 @@
+const quiz =[
+    {
+        question:"What is shortform of Hypertext Markup Language",
+        a:"HTML",
+        b:"HTMLT",
+        c:"MLHT",
+        
+        correct:"HTML",
+    },
+    {
+        question:"What is the other name for Javascript",
+        a:"ECMAscript",
+        b:"JS",
+        c:"none",
+        Correct:"Js",
+    },
+    {
+        question:"Is c++ a low-level language",
+        a:"True",
+        b:"False",
+        c:"none",
+        correct:"True",
+    },
+    {
+        question:"Is c++ a low-level language",
+        a:"True",
+        b:"False",
+        c:"none",
+        correct:"True",
+    }
+];
